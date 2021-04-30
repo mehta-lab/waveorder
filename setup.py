3e606d2db92ddb8235e4440f481d0946b2046f07
Bryant <bchhun@gmail.com>
2021-04-29 17:21:34 -0700

    setup for pytest license doc

--- setup.py
+++ setup.py
@@ -1,11 +1,67 @@
+import os.path as osp
 from setuptools import setup, find_packages
 
-setup(  name             = 'waveorder',
-        version          = '0.0.1',
-        description      = 'functions for reconstructing and visualizing phase and birefrigence',
-        author           = 'Li-Hao Yeh',
-        author_email     = 'lihao.yeh@czbiohub.org',
-        license          = 'BSD License',
-        packages         = find_packages(),
-        install_requires = ['numpy', 'matplotlib', 'scipy', 'ipywidgets']
-     )
+DISTNAME = 'waveorder'
+VERSION = '0.0.1'
+DESCRIPTION = 'functions for reconstructing and visualizing phase and birefrigence'
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    LONG_DESCRIPTION_content_type = "text/markdown"
+    # LONG_DESCRIPTION = __doc__
+LICENSE = 'Chan Zuckerberg Biohub Software License'
+DOWNLOAD_URL = 'https://github.com/mehta-lab/waveorder'
+MIN_PY_VER = '3.7'
+
+CLASSIFIERS = [
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Utilities',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS'
+]
+
+# populate packages
+PACKAGES = [package for package in find_packages()]
+
+# parse requirements
+with open(osp.join('requirements', 'default.txt')) as f:
+    requirements = [line.strip() for line in f
+                    if line and not line.startswith('#')]
+
+# populate requirements
+INSTALL_REQUIRES = []
+REQUIRES = []
+for l in requirements:
+    sep = l.split(' #')
+    INSTALL_REQUIRES.append(sep[0].strip())
+    if len(sep) == 2:
+        REQUIRES.append(sep[1].strip())
+
+if __name__ == '__main__':
+    setup(
+        name=DISTNAME,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_content_type,
+        license=LICENSE,
+        download_url=DOWNLOAD_URL,
+        version="0.0.1",
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        requires=REQUIRES,
+        python_requires=f'>={MIN_PY_VER}',
+        packages=PACKAGES,
+        include_package_data=True,
+        # entry_points={
+        #         'console_scripts': ['runReconstruction=ReconstructOrder.cli_module:main']
+        # }
+    )
