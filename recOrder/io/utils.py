b3bb251536765eb3ef71d65a9be58b1f6b967cb6
Cameron Foltz <camfoltz2@gmail.com>
2021-05-10 20:00:59 -0700

    fix background load bug

--- recOrder/io/utils.py
+++ recOrder/io/utils.py
@@ -27,9 +27,8 @@ def load_bg(bg_path, height, width, ROI=None):
     bg_data   : (ndarray) Array of background data w/ dimensions (N_channel, Y, X)
     """
 
-    bg_paths = glob.glob(bg_path + '*.tif')
+    bg_paths = glob.glob(os.path.join(bg_path,'*.tif'))
     bg_paths.sort()
-    #     print(bg_paths)
     bg_data = np.zeros([len(bg_paths), height, width])
 
     for i in range(len(bg_paths)):
