2db77bacc9c6e0935051391bea9d596faa3ee5c9
Cameron Foltz <camfoltz2@gmail.com>
2021-05-10 17:21:03 -0700

    add datatype to reader

--- recOrder/pipelines/run_pipeline.py
+++ recOrder/pipelines/run_pipeline.py
@@ -6,6 +6,6 @@ from recOrder.io.config_reader import ConfigReader
 def run_pipeline(config: ConfigReader):
 
     for sample in config.samples:
-        data = MicromanagerReader(os.path.join(config.data_dir, sample))
+        data = MicromanagerReader(os.path.join(config.data_dir, sample, config.data_type))
         pipeline = PipelineConstructor(config, data, sample)
         pipeline.run()
