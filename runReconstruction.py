e9b13539dee1c728d191d1af1ef37486e7ab5995
Cameron Foltz <camfoltz2@gmail.com>
2021-05-14 09:14:16 -0700

    switch back to argparse

--- runReconstruction.py
+++ runReconstruction.py
@@ -11,35 +11,50 @@ Returns
 """
 
 import click
+import argparse
 import shutil
 import os
 from recOrder.pipelines.run_pipeline import run_pipeline
 from recOrder.io.config_reader import ConfigReader
 
 
-# @click.group(invoke_without_command=True)
-# @click.option('--option1')
-# @click.argument('arg1')
-
-
+# @click.command()
 # @click.option('--config', required=True, type=str, help='path to config yml file')
-# @click.group(invoke_without_command=True)
-@click.command()
-@click.option('--config', required=True, type=str, help='path to config yml file')
-def parse_args(config):
+# def parse_args(config):
+#     """Parse command line arguments
+#     In python namespaces are implemented as dictionaries
+#     :return: namespace containing the arguments passed.
+#     """
+#
+#     if os.path.exists(config):
+#         return config
+#
+#     else:
+#         raise ValueError('Specified path does not exist')
+
+
+def parse_args():
     """Parse command line arguments
     In python namespaces are implemented as dictionaries
     :return: namespace containing the arguments passed.
     """
 
-    if os.path.exists(config):
-        return config
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--config', type=str,
+                       help='path to yaml configuration file')
+
+    args = parser.parse_args()
+
+    if os.path.exists(args):
+        return args
 
     else:
         raise ValueError('Specified path does not exist')
 
+
 if __name__ == '__main__':
-    cfg_path = parse_args(standalone_mode=True)
-    config = ConfigReader(cfg_path)
+    args = parse_args()
+    config = ConfigReader(args)
     run_pipeline(config)
-    shutil.copy(cfg_path, os.path.join(config.processed_dir, 'config.yml'))
+    shutil.copy(args, os.path.join(config.processed_dir, 'config.yml'))
