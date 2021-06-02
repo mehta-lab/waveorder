5a2e8c1c7fba5571d899590685db1deab54a09cf
Cameron Foltz <camfoltz2@gmail.com>
2021-06-02 07:35:12 -0700

    make config reader changes

--- recOrder/io/config_reader2.py
+++ recOrder/io/config_reader2.py
@@ -3,68 +3,70 @@ import pathlib
 import warnings
 
 DATASET = {
-    ('method', None),
-    ('mode', None),
-    ('data_dir', None),
-    ('save_dir', None),
-    ('data_save_name', None),
-    ('positions', ['all']),
-    ('timepoints', ['all']),
-    ('background', None),
-    ('background_ROI', None),
-    ('calibration_metadata', None)
+    'method': None,
+    'mode': None,
+    'data_dir': None,
+    'save_dir': None,
+    'data_save_name': None,
+    'positions': ['all'],
+    'timepoints': ['all'],
+    'background': None,
+    'background_ROI': None,
+    'calibration_metadata': None
 }
 
 PREPROCESSING = {
-    ('denoise',
-        [('use', False),
-         ('channels', None),
-         ('threshold',None),
-         ('level', None)])
+    'denoise': {
+        'use': False,
+        'channels': None,
+        'threshold': None,
+        'level': None
+    }
 }
 
 PROCESSING = {
-    ('output_channels', None),
-    ('background_correction', 'None'),
-    ('flatfield_correction', None),
-    ('use_gpu', False),
-    ('gpu_id', 0),
-    ('wavelength', None),
-    ('pixel_size', None),
-    ('magnification', None),
-    ('NA_objective', None),
-    ('NA_condenser', None),
-    ('n_objective_media', 1.003),
-    ('z_step', None),
-    ('focus_zidx', None),
-    ('phase_denoiser_2D', 'Tikhonov'),
-    ('Tik_reg_abs_2D', 1e-4),
-    ('Tik_reg_ph_2D', 1e-4),
-    ('rho_2D', 1),
-    ('itr_2D', 50),
-    ('TV_reg_abs_2D', 1e-4),
-    ('TV_reg_ph_2D', 1e-4),
-    ('phase_denoiser_3D', 'Tikhonov'),
-    ('rho_3D', 1e-3),
-    ('itr_3D', 50),
-    ('Tik_reg_ph_3D', 1e-4),
-    ('TV_reg_ph_3D', 5e-5),
-    ('pad_z', 0)
+    'output_channels': None,
+    'background_correction': 'None',
+    'flatfield_correction': None,
+    'use_gpu': False,
+    'gpu_id': 0,
+    'wavelength':None,
+    'pixel_size': None,
+    'magnification': None,
+    'NA_objective': None,
+    'NA_condenser': None,
+    'n_objective_media': 1.003,
+    'z_step': None,
+    'focus_zidx': None,
+    'phase_denoiser_2D': 'Tikhonov',
+    'Tik_reg_abs_2D': 1e-4,
+    'Tik_reg_ph_2D': 1e-4,
+    'rho_2D': 1,
+    'itr_2D': 50,
+    'TV_reg_abs_2D': 1e-4,
+    'TV_reg_ph_2D': 1e-4,
+    'phase_denoiser_3D': 'Tikhonov',
+    'rho_3D': 1e-3,
+    'itr_3D': 50,
+    'Tik_reg_ph_3D': 1e-4,
+    'TV_reg_ph_3D': 5e-5,
+    'pad_z': 0
 }
 
 
 
 POSTPROCESSING = {
-    ('denoise',
-        [('use', False),
-         ('channels', None),
-         ('threshold',None),
-         ('level', None)]),
+    'denoise':{
+        'use': False,
+        'threshold': None,
+        'level': None
+    },
 
-    ('registration',
-         [('use', False),
-          ('channel_idx', None),
-          ('shift', None)])
+    'registration':{
+        'use': False,
+        'channel_idx': None,
+        'shift': None
+    }
 }
 
 class Object():
@@ -130,7 +132,7 @@ class ConfigReader(object):
 
     def _parse_dataset(self):
         for key, value in self.config['dataset'].items():
-            if key in DATASET:
+            if key in DATASET.keys():
 
                 # if config has a data_save name, but a user specifies a different name in CLI,
                 # skip and use the user-specified name
@@ -158,28 +160,29 @@ class ConfigReader(object):
     #TODO: MAKE COMPATIBLE WITH PREDEFINED LIST
     def _parse_preprocessing(self):
         for key, value in self.config['preprocessing'].items():
-            if key in PREPROCESSING:
+            if key in PREPROCESSING.keys():
                 for key_child, value_child in self.config['preprocessing'][key]:
-                    setattr(self.preprocessing, f'key_{key_child}', value_child)
-                else:
-                    warnings.warn(f'yaml PREPROCESSING config field {key}, {key_child} is not recognized')
+                    if key_child in key.keys():
+                        setattr(self.preprocessing, f'{key}_{key_child}', value_child)
+                    else:
+                        warnings.warn(f'yaml PREPROCESSING config field {key}, {key_child} is not recognized')
             else:
                 warnings.warn(f'yaml PREPROCESSING config field {key} is not recognized')
 
     def _parse_processing(self):
         for key, value in self.config['dataset'].items():
-            if key in PROCESSING:
+            if key in PROCESSING.keys():
                 setattr(self, key, value)
             else:
                 warnings.warn(f'yaml PROCESSING config field {key} is not recognized')
 
     def _parse_postprocessing(self):
         for key, value in self.config['postprocessing'].items():
-            if key in POSTPROCESSING:
+            if key in POSTPROCESSING.keys():
                 for key_child, value_child in self.config['postprocessing'][key]:
-                    if f'{key}_{key_child}' in
-                    setattr(self.postprocessing, f'key_{key_child}', value_child)
-                else:
-                    warnings.warn(f'yaml POSTPROCESSING config field {key}, {key_child} is not recognized')
+                    if key_child in key.keys():
+                        setattr(self.postprocessing, f'{key}_{key_child}', value_child)
+                    else:
+                        warnings.warn(f'yaml POSTPROCESSING config field {key}, {key_child} is not recognized')
             else:
                 warnings.warn(f'yaml POSTPROCESSING config field {key} is not recognized')
