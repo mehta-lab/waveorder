73e9e147560aaef4be5afbe20997192be49c98a9
Cameron Foltz <camfoltz2@gmail.com>
2021-06-07 14:14:32 -0700

    update flags for processing

--- recOrder/io/config_reader.py
+++ recOrder/io/config_reader.py
@@ -156,22 +156,16 @@ class ConfigReader(object):
         if not name: assert 'save_dir' in self.config['dataset'], \
             'Please provide data_save_name in config file or CLI argument'
 
-        # for key,value in PROCESSING.items():
-        #     if key == 'output_channels':
-        #         if 'Phase3D' in self.config['processing'][key] or 'Phase2D' in self.config['processing'][key]:
-        #             'wavelength': None,
-        #             'pixel_size': None,
-        #             'magnification': None,
-        #             'NA_objective': None,
-        #             'NA_condenser': None,
-        #             phase_processing = True
-        #         elif 'Phase3D' in self.config['processing'][key] and 'Phase2D' in self.config['processing'][key]:
-        #             raise KeyError(f'Both Phase3D and Phase2D cannot be specified in {key}')
-        #     if phase_processing:
-        #         assert('')
+        for key,value in PROCESSING.items():
+            if key == 'output_channels':
+                if 'Phase3D' in self.config['processing'][key] or 'Phase2D' in self.config['processing'][key]:
+                    assert 'wavelength' in self.config['processing'], 'wavelength required for phase reconstruction'
+                    assert 'pixel_size' in self.config['processing'], 'pixel_size required for phase reconstruction'                    'magnification': None,
+                    assert 'NA_objective' in self.config['processing'], 'NA_objective required for phase reconstruction'
+                    assert 'NA_condenser' in self.config['processing'], 'NA_condenser required for phase reconstruction'
 
-
-
+                elif 'Phase3D' in self.config['processing'][key] and 'Phase2D' in self.config['processing'][key]:
+                    raise KeyError(f'Both Phase3D and Phase2D cannot be specified in {key}')
 
         if 'preprocessing' in self.config:
             for key, value in PREPROCESSING.items():
