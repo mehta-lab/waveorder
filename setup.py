a3eb1260e3576f33ebd04b02ccc1c06c7aa5d5a0
camfoltz <camfoltz2@gmail.com>
2021-10-27 16:49:18 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
         include_package_data=True,
         entry_points={
             'console_scripts': [
-                'recOrder.reconstruct = recOrder.cli_module:main',
+                'recOrder.reconstruct = scripts.run_pipeline:main',
                 'recOrder.convert = scripts.convert_tiff_to_zarr:main'
             ]
         }
