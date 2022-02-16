f6c23a62075b4ced16c4b9fefe1fb34221f15d25
camfoltz <camfoltz2@gmail.com>
2022-02-15 16:47:22 -0800

    refactor and make available as napari plugin

--- recOrder/__init__.py
+++ recOrder/__init__.py
@@ -1,3 +1,4 @@
 #todo: format overall init
 
 # from .plugin.widget.napari_plugin_entry_point import napari_experimental_provide_dock_widget
+# name = "recOrder"
--- setup.py
+++ setup.py
@@ -1,74 +1,8 @@
-import os.path as osp
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-# todo: modify as we decide on versions, names, descriptions. readme
-MIN_PY_VER = '3.7'
-DISTNAME = 'recOrder'
-DESCRIPTION = 'computational microscopy toolkit for label-free imaging'
-with open("README.md", "r") as fh:
-    LONG_DESCRIPTION = fh.read()
-    LONG_DESCRIPTION_content_type = "text/markdown"
-    LONG_DESCRIPTION = __doc__
-LICENSE = 'Chan Zuckerberg Biohub Software License'
 
-INSTALL_REQUIRES = ['numpy', 'scipy', 'matplotlib', 'pycromanager']
-REQUIRES = []
-
-# todo: modify for python dependency
-CLASSIFIERS = [
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Topic :: Scientific/Engineering',
-    'Topic :: Scientific/Engineering :: Visualization',
-    'Topic :: Scientific/Engineering :: Information Analysis',
-    'Topic :: Scientific/Engineering :: Bio-Informatics',
-    'Topic :: Utilities',
-    'Framework :: napari',
-    'Operating System :: Microsoft :: Windows',
-    'Operating System :: POSIX',
-    'Operating System :: Unix',
-    'Operating System :: MacOS'
-]
-
-# populate packages
-PACKAGES = [package for package in find_packages()]
-
-# parse requirements
-with open(osp.join('requirements', 'default.txt')) as f:
-    requirements = [line.strip() for line in f
-                    if line and not line.startswith('#')]
-
-# populate requirements
-for l in requirements:
-    sep = l.split(' #')
-    INSTALL_REQUIRES.append(sep[0].strip())
-    if len(sep) == 2:
-        REQUIRES.append(sep[1].strip())
-
-if __name__ == '__main__':
-    setup(
-        name=DISTNAME,
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        long_description_content_type=LONG_DESCRIPTION_content_type,
-        license=LICENSE,
-        version="0.0.1",
-        classifiers=CLASSIFIERS,
-        install_requires=INSTALL_REQUIRES,
-        python_requires=f'>={MIN_PY_VER}',
-        dependency_links=['https://github.com/mehta-lab/waveorder.git#egg=waveorder'],
-        packages=PACKAGES,
-        include_package_data=True,
-        entry_points={
-            'console_scripts': [
-                'recOrder.help = scripts.help:main',
-                'recOrder.reconstruct = scripts.run_pipeline:main',
-                'recOrder.convert = scripts.convert_tiff_to_zarr:main',
-                'recOrder = scripts.launch_napari:main'
-            ],
-            'napari.plugin': ['recorder = recOrder']
-        }
-    )
+# https://github.com/pypa/setuptools_scm
+use_scm = {"write_to": "recOrder/_version.py"}
+setup(use_scm_version=use_scm)
