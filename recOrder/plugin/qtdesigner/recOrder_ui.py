b4b7cbb76bd9fc66847abde97ef88e4d1c3033da
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2022-08-04 11:24:10 -0700

    update ToolTipRole info

--- recOrder/plugin/qtdesigner/recOrder_ui.py
+++ recOrder/plugin/qtdesigner/recOrder_ui.py
@@ -1722,12 +1722,12 @@ class Ui_Form(object):
         self.cb_calib_mode.setItemText(1, _translate("Form", "MM-Voltage"))
         self.cb_calib_mode.setItemText(2, _translate("Form", "DAC"))
         self.cb_calib_mode.setItemData(0, "The retardance of the LC is set directly through the MicroManager LC device\n"
-                                          "adapter. The MicroManager device adapter determines the corresponding\n"
-                                          "voltage which is sent to the LC", QtCore.Qt.ToolTipRole)
+                                          "adapter. The device adapter determines the corresponding voltage which is\n"
+                                          "sent to the LC", QtCore.Qt.ToolTipRole)
         self.cb_calib_mode.setItemData(1, "The CalibrationData class in recOrder uses the LC calibration data to\n"
                                           "determine the correct LC voltage for a given retardance. The LC voltage is\n"
                                           "set through the MicroManager LC device adapter", QtCore.Qt.ToolTipRole)
-        self.cb_calib_mode.setItemData(2, "the CalibrationData class in recOrder uses the LC calibration data to\n"
+        self.cb_calib_mode.setItemData(2, "The CalibrationData class in recOrder uses the LC calibration data to\n"
                                           "determine the correct LC voltage for a given retardance. The voltage is\n"
                                           "applied to the IO port of the LC controller through the TriggerScope DAC "
                                           "outputs", QtCore.Qt.ToolTipRole)
