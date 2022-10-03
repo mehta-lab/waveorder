6f82340cdbb8ea9ce3bbf2bb314c1641da5c4289
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2022-10-03 13:36:48 -0700

    avoid overriding `recorder`

--- recOrder/scripts/repeat-cal-acq-rec.py
+++ recOrder/scripts/repeat-cal-acq-rec.py
@@ -14,33 +14,33 @@ bkg_repeats = 3
 
 def main():
     viewer = napari.Viewer()
-    recorder = MainWidget(viewer)
-    viewer.window.add_dock_widget(recorder)
-    recorder.ui.qbutton_gui_mode.click()
-    recorder.calib_scheme = '5-State'
-    recorder.directory = save_dir
-    recorder.save_directory = save_dir
+    app = MainWidget(viewer)
+    viewer.window.add_dock_widget(app)
+    app.ui.qbutton_gui_mode.click()
+    app.calib_scheme = '5-State'
+    app.directory = save_dir
+    app.save_directory = save_dir
 
     input("Move to background and press <Enter>")
     for cal_repeat in range(cal_repeats):
         print(f"Calibration repeat # {cal_repeat}")
-        recorder.swing = swing
+        app.swing = swing
     
         print(f"Calibrating with swing = {swing}")
-        recorder.run_calibration()
+        app.run_calibration()
         time.sleep(90)
  
         for bkg_repeat in range(bkg_repeats):
             print(f"Background repeat # {bkg_repeat}")
-            recorder.last_calib_meta_file = recorder.calib.meta_file
-            recorder.capture_bg()
+            app.last_calib_meta_file = app.calib.meta_file
+            app.capture_bg()
             time.sleep(20)
         
             input("Move to target and press <Enter>")
-            recorder.ui.cb_bg_method.setCurrentIndex(1) # Set to "Measured" bg correction
-            recorder.enter_bg_correction()
-            recorder.save_name = f"cal-{cal_repeat}-bkg-{bkg_repeat}"
-            recorder.acq_birefringence()
+            app.ui.cb_bg_method.setCurrentIndex(1) # Set to "Measured" bg correction
+            app.enter_bg_correction()
+            app.save_name = f"cal-{cal_repeat}-bkg-{bkg_repeat}"
+            app.acq_birefringence()
             time.sleep(15)
 
             input("Move to background and press <Enter>")
