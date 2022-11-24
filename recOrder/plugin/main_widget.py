57be9d1869076805066689f43118ae422216321c
Talon Chandler <talonchandler@gmail.com>
2022-11-23 17:09:12 -0800

    Better comment

--- recOrder/plugin/main_widget.py
+++ recOrder/plugin/main_widget.py
@@ -898,6 +898,7 @@ class MainWidget(QWidget):
                 self.ui.cb_config_group.addItem(group)
 
             # Populate the BF channel list with presets that contain any of these keywords
+            # Populate the acquisition "BF channel" list with presets that contain any of these keywords
             bf_keywords = ["bf", "brightfield", "bright", "labelfree", "label-free", "lf", "label", ]
             for ch in config_list:
                 if any([keyword.lower() in ch.lower() for keyword in bf_keywords]):
