1d039304ad1da95cf9ac8b18b2b9ed8af83c9e7e
Talon Chandler <talonchandler@gmail.com>
2023-04-14 16:38:56 -0700

    phase3Dto3D complete overhaul

---
+++ examples/models/phase3Dto3D.py
@@ -0,0 +1,69 @@
+# 3D partially coherent optical diffraction tomography (ODT) simulation                                                                  #
+# ```J. M. Soto, J. A. Rodrigo, and T. Alieva, "Label-free quantitative 3D tomographic imaging for partially    #
+# coherent light microscopy," Opt. Express 25, 15699-15712 (2017).```                                           #
+
+import torch
+import napari
+import numpy as np
+from waveorder import util
+from waveorder.models import iso3Dto3D
+
+v = napari.Viewer()
+
+# Experiment parameters
+tf_params = {
+    "ZYX_shape": (100, 256, 256),  # simulation size
+    "YX_ps": 6.5 / 63,  # effective pixel size
+    "Z_ps": 0.25,  # axial pixel size
+    "Z_pad": 0,  # paddding
+    "lamb_ill": 0.532,  # wavelength
+    "n_media": 1.3,  # refractive index in the media
+    "NA_ill": 0.9,  # illumination NA
+    "NA_obj": 1.2,  # objective NA
+}
+
+# Generate a phantom
+n_sample = 1.50
+radius = 5
+blur_size = 2 * tf_params["YX_ps"]
+sphere, _, _ = util.gen_sphere_target(
+    tf_params["ZYX_shape"],
+    tf_params["YX_ps"],
+    tf_params["Z_ps"],
+    radius,
+    blur_size,
+)
+RI_map = torch.zeros_like(sphere)
+RI_map[sphere > 0] = sphere[sphere > 0] * (n_sample - tf_params["n_media"])
+RI_map += tf_params["n_media"]
+t_obj = torch.exp(
+    1j * 2 * np.pi * tf_params["Z_ps"] * (RI_map - tf_params["n_media"])
+)
+
+# Display input bead
+ZYX_scale = np.array(
+    [tf_params["Z_ps"], tf_params["YX_ps"], tf_params["YX_ps"]]
+)
+v.add_image(torch.angle(t_obj).numpy(), name="Phantom", scale=ZYX_scale)
+input("Showing input phantom. Press <enter> to continue...")
+v.layers.remove("Phantom")
+
+# Calculate and display OTF
+H_re, H_im = iso3Dto3D.calc_TF(**tf_params)
+iso3Dto3D.visualize_TF(v, H_re, H_im, ZYX_scale)
+input("Showing OTFs. Press <enter> to continue...")
+v.layers.remove("Re(H_im)")
+v.layers.remove("Im(H_im)")
+v.layers.remove("Re(H_re)")
+v.layers.remove("Im(H_re)")
+
+# Perform simulation, reconstruction, and display both
+ZYX_data = iso3Dto3D.apply_TF(t_obj, H_re)
+ZYX_recon = iso3Dto3D.apply_inv_TF(
+    ZYX_data, H_re, H_im, tf_params["Z_ps"], tf_params["lamb_ill"]
+)
+
+v.add_image(torch.angle(t_obj).numpy(), name="Phantom", scale=ZYX_scale)
+v.add_image(ZYX_data.numpy(), name="Data", scale=ZYX_scale)
+v.add_image(ZYX_recon.numpy(), name="Reconstruction", scale=ZYX_scale)
+input("Showing object, data, and recon. Press <enter> to quit...")
---
+++ waveorder/models/phase3Dto3D.py
@@ -0,0 +1,114 @@
+import numpy as np
+import torch
+from waveorder import optics, util
+
+
+def calc_TF(
+    ZYX_shape,
+    YX_ps,
+    Z_ps,
+    Z_pad,
+    lamb_ill,
+    n_media,
+    NA_ill,
+    NA_obj,
+):
+    frr = util.gen_radial_freq(ZYX_shape[1:], YX_ps)
+    Z_total = ZYX_shape[0] + 2 * Z_pad
+    Z_pos_list = torch.fft.ifftshift(
+        (torch.arange(Z_total) - Z_total // 2) * Z_ps
+    )
+
+    ill_pupil = optics.gen_pupil(frr, NA_ill, lamb_ill)
+    det_pupil = optics.gen_pupil(frr, NA_obj, lamb_ill)
+    Hz_stack = optics.gen_Hz_stack(
+        frr, det_pupil, lamb_ill / n_media, Z_pos_list
+    )
+    G_fun_z_3D = optics.gen_Greens_function_z(
+        frr, det_pupil, lamb_ill / n_media, Z_pos_list
+    )
+
+    H_re, H_im = optics.WOTF_3D_compute(
+        ill_pupil, ill_pupil, det_pupil, Hz_stack, G_fun_z_3D, Z_ps
+    )
+
+    return H_re, H_im
+
+
+def visualize_TF(viewer, H_re, H_im, ZYX_scale):
+    arrays = [
+        (torch.real(H_im).numpy(), "Re(H_im)"),
+        (torch.imag(H_im).numpy(), "Im(H_im)"),
+        (torch.real(H_re).numpy(), "Re(H_re)"),
+        (torch.imag(H_re).numpy(), "Im(H_re)"),
+    ]
+
+    for array in arrays:
+        lim = 0.5 * np.max(np.abs(array[0]))
+        viewer.add_image(
+            np.fft.ifftshift(array[0]),
+            name=array[1],
+            colormap="bwr",
+            contrast_limits=(-lim, lim),
+            scale=1 / ZYX_scale,
+        )
+    viewer.dims.order = (2, 0, 1)
+
+
+def apply_TF(ZYX_obj, H_re):
+    # TODO: extend to absorption, or restrict to just phase
+    ZYX_obj_hat = torch.fft.fftn(ZYX_obj)
+    ZYX_data = ZYX_obj_hat * H_re
+    return torch.real(torch.fft.ifftn(ZYX_data))
+
+
+def apply_inv_TF(
+    ZYX_data,
+    H_re,
+    H_im,
+    Z_ps,  # TODO: MOVE THIS PARAM TO OTF
+    lamb_ill,  # TOOD: MOVE THIS PARAM TO OTF
+    absorption_ratio=0.0,
+    method="Tikhonov",
+    **kwargs
+):
+    # # TODO HANDLE PADDING IN A SIMPLE WAY
+    # pad_z = H_re.shape[0] - ZYX_data.shape[0]
+
+    # if pad_z < 0:
+    #     raise ("")
+    # elif pad_z == 0:
+    #     S0_stack = util.inten_normalization_3D(ZYX_data)
+    # elif pad_z >= 0:  ## FINISH THIS
+    #     S0_pad = np.pad(
+    #         ZYX_data,
+    #         ((pad_z, pad_z), (0, 0), (0, 0)),
+    #         mode="constant",
+    #         constant_values=0,
+    #     )
+    #     if pad_z < ZYX_data.shape[0]:
+    #         S0_pad[:pad_z, :, :] = (S0_stack[:pad_z, :, :])[::-1, :, :]
+    #         S0_pad[-pad_z:, :] = (S0_stack[-pad_z:, :])[::-1, :, :]
+    #     else:
+    #         print(
+    #             "pad_z is larger than number of z-slices, use zero padding (not effective) instead of reflection padding"
+    #         )
+
+    ZYX_data = util.inten_normalization_3D(ZYX_data)
+
+    H_eff = H_re + absorption_ratio * H_im
+
+    if method == "Tikhonov":
+        f_real = util.Single_variable_Tikhonov_deconv_3D(
+            ZYX_data, H_eff, **kwargs
+        )
+
+    elif method == "TV":
+        f_real = util.Single_variable_ADMM_TV_deconv_3D(
+            ZYX_data, H_eff, **kwargs
+        )
+
+    # if Z_pad != 0:
+    #    f_real = f_real[pad_z:-pad_z, ...]
+
+    return f_real * Z_ps / 4 / np.pi * lamb_ill
