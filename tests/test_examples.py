822a45eb992170957a47fb7e8b5696fdf9502f36
Talon Chandler <talonchandler@gmail.com>
2023-04-18 18:28:24 -0700

    update tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,9 +4,40 @@ import pytest
 import sys
 
 
+def _run_scripts(scripts):
+    for script in scripts:
+        path = os.path.join(os.getcwd(), "examples/", script)
+        completed_process = subprocess.run(["python", path])
+        assert completed_process.returncode == 0
+
+
+# Currently, these tests will run in the background if no screen is available
+# If you do have a screen available, plots will appear, and you'll need to
+# close them to make the tests pass.
+# TODO: see if we can make these run locally w/o showing matplotlib
+# @patch("matplotlib.pyplot.show")
+def test_qlipp_examples():
+    scripts = [
+        "QLIPP_simulation/2D_QLIPP_forward.py",
+        "QLIPP_simulation/2D_QLIPP_recon.py",
+    ]
+    _run_scripts(scripts)
+
+
+def test_pti_examples():
+    scripts = [
+        "PTI_simulation/PTI_Simulation_Forward_2D3D.py",
+        "PTI_simulation/PTI_Simulation_Recon2D.py",
+        "PTI_simulation/PTI_Simulation_Recon3D.py",
+    ]
+    _run_scripts(scripts)
+
+
 @pytest.mark.skipif("napari" not in sys.modules, reason="requires napari")
-def test_example_phase3Dto3D():
-    path = os.path.join(os.getcwd(), "examples/models/phase3Dto3D.py")
+def test_phase_examples():
+    path = os.path.join(
+        os.getcwd(), "examples/PODT_phase_simulation/phase3Dto3D.py"
+    )
     # examples needs two <enters>s so send input="e\ne"
     completed_process = subprocess.run(
         ["python", path], input="e\ne", encoding="ascii"
