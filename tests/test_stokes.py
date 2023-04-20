5195f813d2ef54268f5aff3508cf603fae246e7f
Talon Chandler <talonchandler@gmail.com>
2023-04-20 12:07:54 -0700

    Rename variables in `stokes.py`.

--- tests/test_stokes.py
+++ tests/test_stokes.py
@@ -5,69 +5,84 @@ import numpy.testing as npt
 
 
 def test_S2I_matrix():
-    S2I5 = stokes.S2I_matrix(0.1)
+    S2I5 = stokes.calculate_stokes_to_intensity_matrix(0.1)
     assert S2I5.shape == (5, 4)
 
-    S2I4 = stokes.S2I_matrix(0.1, scheme="4-State")
+    S2I4 = stokes.calculate_stokes_to_intensity_matrix(0.1, scheme="4-State")
     assert S2I4.shape == (4, 4)
 
-    npt.assert_almost_equal(stokes.S2I_matrix(0), stokes.S2I_matrix(1))
+    npt.assert_almost_equal(
+        stokes.calculate_stokes_to_intensity_matrix(0),
+        stokes.calculate_stokes_to_intensity_matrix(1),
+    )
 
     with pytest.raises(ValueError):
-        A2Ix = stokes.S2I_matrix(0.1, scheme="3-State")
+        A2Ix = stokes.calculate_stokes_to_intensity_matrix(
+            0.1, scheme="3-State"
+        )
 
 
 def test_I2S_matrix():
-    I2S5 = stokes.I2S_matrix(0.1)
+    I2S5 = stokes.calculate_intensity_to_stokes_matrix(0.1)
     assert I2S5.shape == (4, 5)
 
-    I = np.matmul(stokes.I2S_matrix(0.1), stokes.S2I_matrix(0.1))
+    I = np.matmul(
+        stokes.calculate_intensity_to_stokes_matrix(0.1),
+        stokes.calculate_stokes_to_intensity_matrix(0.1),
+    )
     npt.assert_almost_equal(I, np.eye(I.shape[0]))
 
 
-def test_s12_to_ori():
-    for ori in np.linspace(0, np.pi, 25, endpoint=False):
-        ori1 = stokes._s12_to_ori(np.sin(2 * ori), -np.cos(2 * ori))
-        assert ori - ori1 < 1e-8
+def test_s12_to_orientation():
+    for orientation in np.linspace(0, np.pi, 25, endpoint=False):
+        orientation1 = stokes._s12_to_orientation(
+            np.sin(2 * orientation), -np.cos(2 * orientation)
+        )
+        assert orientation - orientation1 < 1e-8
 
 
 def test_stokes_recon():
-
-    # NOTE: skip retardance = 0 and dop = 0 because orientation is not defined
-    for ret in np.arange(1e-3, 1, 0.1):  # fractions of a wave
-        for ori in np.arange(0, np.pi, np.pi / 10):  # radians
-            for tra in [0.1, 10]:
-
-                # Test attenuating retarder (AR) functions
-                AR = (ret, ori, tra)
-                s012 = stokes.stokes012_after_AR(*AR)
-                AR1 = stokes.estimate_AR_from_stokes012(*s012)
+    # NOTE: skip retardance = 0 and depolarization = 0 because orientationentation is not defined
+    for retardance in np.arange(1e-3, 1, 0.1):  # fractions of a wave
+        for orientation in np.arange(0, np.pi, np.pi / 10):  # radians
+            for transmittance in [0.1, 10]:
+                # Test attenuating retarder (ar) functions
+                ar = (retardance, orientation, transmittance)
+                s012 = stokes.stokes012_after_ar(*ar)
+                ar1 = stokes.estimate_ar_from_stokes012(*s012)
                 for i in range(3):
-                    assert AR[i] - AR1[i] < 1e-8
+                    assert ar[i] - ar1[i] < 1e-8
 
-                # Test attenuating depolarizing retarder (ADR) functions
-                for dop in np.arange(1e-3, 1, 0.1):
-                    ADR = (ret, ori, tra, dop)
-                    s0123 = stokes.stokes_after_ADR(*ADR)
-                    ADR1 = stokes.estimate_ADR_from_stokes(*s0123)
+                # Test attenuating depolarizing retarder (adr) functions
+                for depolarization in np.arange(1e-3, 1, 0.1):
+                    adr = (
+                        retardance,
+                        orientation,
+                        transmittance,
+                        depolarization,
+                    )
+                    s0123 = stokes.stokes_after_adr(*adr)
+                    adr1 = stokes.estimate_adr_from_stokes(*s0123)
 
                     for i in range(4):
-                        assert ADR[i] - ADR1[i] < 1e-8
+                        assert adr[i] - adr1[i] < 1e-8
 
 
-def test_stokes_after_ADR_usage():
-    x = stokes.stokes_after_ADR(1, 1, 1, 1)
+def test_stokes_after_adr_usage():
+    x = stokes.stokes_after_adr(1, 1, 1, 1)
 
     ret = np.ones((2, 3, 4, 5))
-    ori = np.ones((2, 3, 4, 5))
-    tra = np.ones((2, 3, 4, 5))
-    dop = np.ones((2, 3, 4, 5))
-    x2 = stokes.stokes_after_ADR(ret, ori, tra, dop)
+    orientation = np.ones((2, 3, 4, 5))
+    transmittance = np.ones((2, 3, 4, 5))
+    depolarization = np.ones((2, 3, 4, 5))
+    x2 = stokes.stokes_after_adr(
+        ret, orientation, transmittance, depolarization
+    )
 
-    ADR_params = np.ones(
+    adr_params = np.ones(
         (4, 2, 3, 4, 5)
     )  # first axis contains the Stokes indices
-    stokes.stokes_after_ADR(*ADR_params)  # * expands along the first axis
+    stokes.stokes_after_adr(*adr_params)  # * expands along the first axis
 
 
 def test_mueller_from_stokes():
@@ -83,7 +98,6 @@ def test_mueller_from_stokes():
 
 
 def test_mmul():
-
     M = np.ones((3, 2, 1))
     x = np.ones((2, 1))
 
@@ -99,7 +113,7 @@ def test_copying():
     b = np.array([1, 1])
     c = np.array([1, 1])
     d = np.array([1, 1])
-    s0, s1, s2, s3 = stokes.stokes_after_ADR(a, b, c, d)
+    s0, s1, s2, s3 = stokes.stokes_after_adr(a, b, c, d)
     s0[0] = 2  # modify the output
     assert c[0] == 1  # check that the input hasn't changed
 
--- waveorder/stokes.py
+++ waveorder/stokes.py
@@ -11,16 +11,16 @@ S2I = S2I_matrix(swing, scheme="5-State")
 I2S = I2S_matrix(swing, scheme="5-State")
 
 2) Forward functions (Stokes parameters following a optical element)
-s0, s1, s2, s3 = stokes_after_ADR(ret, ori, tra, dop, input="CPL")
-s0, s1, s2 = stokes012_after_AR(ret, ori, tra, input="CPL")
+s0, s1, s2, s3 = stokes_after_adr(retardance, orientation, transmittance, depolarization, input="cpl")
+s0, s1, s2 = stokes012_after_ar(retardance, orientation, transmittance, input="cpl")
 
 3) Inverse functions (optical elements from Stokes parameters)
-ret, ori, tra, dop = estimate_ADR_from_stokes(s0, s1, s2, s3, input="CPL")
-ret, ori, tra = estimate_AR_from_stokes012(s0, s1, s2, input="CPL")
+retardance, orientation, transmittance, depolarization = estimate_adr_from_stokes(s0, s1, s2, s3, input="cpl")
+retardance, orientation, transmittance = estimate_ar_from_stokes012(s0, s1, s2, input="cpl")
 
 4) A function for recovering Mueller matrices from Stokes vector
 M = mueller_from_stokes(
-    s0, s1, s2, s3, model="AR", direction="forward", input="CPL"
+    s0, s1, s2, s3, model="ar", direction="forward", input="cpl"
 )
 
 5) A convenience function for applying Mueller and instrument matrices
@@ -32,24 +32,24 @@ Usage
 All functions are intended to be used with ND-arrays with Stokes- or 
 Mueller-indices as the first axes. 
 
-For example, the following usage modes of stokes_after_ADR are valid:
+For example, the following usage modes of stokes_after_adr are valid:
 
->>> stokes_after_ADR(1, 1, 1, 1)
+>>> stokes_after_adr(1, 1, 1, 1)
 
->>> ret = np.ones((2,3,4,5))
->>> ori = np.ones((2,3,4,5))
->>> tra = np.ones((2,3,4,5))
->>> dop = np.ones((2,3,4,5))
->>> stokes_after_ADR(ret, ori, tra, dop)
+>>> retardance = np.ones((2,3,4,5))
+>>> orientation = np.ones((2,3,4,5))
+>>> transmittance = np.ones((2,3,4,5))
+>>> depolarization = np.ones((2,3,4,5))
+>>> stokes_after_adr(retardance, orientation, transmittance, depolarization)
 
->>> ADR_params = np.ones((4,2,3,4,5)) # first axis contains the Stokes indices
->>> stokes_after_ADR(*ADR_params) # * expands along the first axis
+>>> adr_params = np.ones((4,2,3,4,5)) # first axis contains the Stokes indices
+>>> stokes_after_adr(*adr_params) # * expands along the first axis
 
 """
 import numpy as np
 
 
-def S2I_matrix(swing, scheme="5-State"):
+def calculate_stokes_to_intensity_matrix(swing, scheme="5-State"):
     """
     Calculate the polarimeter system matrix for a swing and calibration scheme.
 
@@ -57,14 +57,15 @@ def S2I_matrix(swing, scheme="5-State"):
     ----------
     swing : float
         Result is periodic on the integers,
-        e.g. S2I_matrix(0.1) = S2I_matrix(1.1)
+        e.g. calculate_stokes_to_intensity_matrix(0.1) ==
+             calculate_stokes_to_intensity_matrix(1.1)
     scheme : "4-State" or "5-State"
         Corresponds to the calibration scheme used to acquire data,
         by default "5-State"
 
     Returns
     -------
-    NDArray
+    NDarray
         Returns different shapes depending on the scheme
 
         S2I.shape = (5, 4) for scheme = "5-State"
@@ -72,7 +73,7 @@ def S2I_matrix(swing, scheme="5-State"):
     """
     chi = 2 * np.pi * swing
     if scheme == "5-State":
-        S2I = np.array(
+        stokes_to_intensity_matrix = np.array(
             [
                 [1, 0, 0, -1],
                 [1, np.sin(chi), 0, -np.cos(chi)],
@@ -83,7 +84,7 @@ def S2I_matrix(swing, scheme="5-State"):
         )
 
     elif scheme == "4-State":
-        S2I = np.array(
+        stokes_to_intensity_matrix = np.array(
             [
                 [1, 0, 0, -1],
                 [1, np.sin(chi), 0, -np.cos(chi)],
@@ -105,10 +106,10 @@ def S2I_matrix(swing, scheme="5-State"):
         raise ValueError(
             f"{scheme} is not implemented, use 4-State or 5-State"
         )
-    return S2I
+    return stokes_to_intensity_matrix
 
 
-def I2S_matrix(swing, scheme="5-State"):
+def calculate_intensity_to_stokes_matrix(swing, scheme="5-State"):
     """
     Calculate the inverse polarimeter system matrix for a swing and calibration
     scheme.
@@ -123,38 +124,42 @@ def I2S_matrix(swing, scheme="5-State"):
 
     Returns
     -------
-    NDArray
+    NDarray
         Returns different shapes depending on the scheme
 
         I2S.shape = (5, 4) for scheme = "5-State"
-        I2S.shape = (4, 4) for scheme = "4-state"
+        I2S.shape = (4, 4) for scheme = "4-State"
     """
-    return np.linalg.pinv(S2I_matrix(swing, scheme=scheme))
+    return np.linalg.pinv(
+        calculate_stokes_to_intensity_matrix(swing, scheme=scheme)
+    )
 
 
-def stokes_after_ADR(ret, ori, tra, dop, input="CPL"):
+def stokes_after_adr(
+    retardance, orientation, transmittance, depolarization, input="cpl"
+):
     """
     Returns the Stokes parameters of the input polarization state (default =
-    circularly polarized light = "CPL") that has passed through an attenuating
-    depolarizing retarder (ADR) parametrized by its retardance (ret), slow-axis
-    orientation (ori), transmittance (tra), and depolarization (dop).
+    circularly polarized light = "cpl") that has passed through an attenuating
+    depolarizing retarder (adr) parametrized by its retardance, slow-axis
+    orientation, transmittance, and depolarization.
 
     Note: all four parameters can be array_like, but they must be the same size.
     If your parameters are in an ndarray with array.shape = (4, ...), use
     the * operator to expand over the first dimension.
 
-    e.g. stokes_after_ADR(*array) is identical to
-    stokes_after_ADR(array[0], array[1], array[2], array[3]).
+    e.g. stokes_after_adr(*array) is identical to
+    stokes_after_adr(array[0], array[1], array[2], array[3]).
 
     Parameters
     ----------
-    ret, ori, tra, dop : array_like, identical shapes
-        ret: retardance of ADR, 2*pi periodic
-        ori: slow-axis orientation of ADR, 2*pi periodic
-        tra: transmittance of ADR, 0 <= tra <= 1
-        dop: depolarization of ADR, 0 <= dop <= 1
+    retardance, orientation, transmittance, depolarization : array_like, identical shapes
+        retardance: retardance of adr, 2*pi periodic
+        orientation: slow-axis orientation of adr, 2*pi periodic
+        transmittance: transmittance of adr, 0 <= transmittance <= 1
+        depolarization: depolarization of adr, 0 <= depolarization <= 1
 
-    input : "CPL"
+    input : "cpl"
         Input polarization state
 
     Returns
@@ -163,23 +168,33 @@ def stokes_after_ADR(ret, ori, tra, dop, input="CPL"):
         Stokes parameters
 
     """
-    if input != "CPL":
-        NotImplementedError("input != CPL")
+    if input != "cpl":
+        NotImplementedError("input != cpl")
 
-    # without copying tra, downstream changes to s0 will affect tra
-    s0 = np.array(tra).copy()
-    s1 = tra * dop * np.sin(ret) * np.sin(2 * ori)
-    s2 = tra * dop * -np.sin(ret) * np.cos(2 * ori)
-    s3 = tra * dop * np.cos(ret)
+    # without copying transmittance, downstream changes to s0 will affect transmittance
+    s0 = np.array(transmittance).copy()
+    s1 = (
+        transmittance
+        * depolarization
+        * np.sin(retardance)
+        * np.sin(2 * orientation)
+    )
+    s2 = (
+        transmittance
+        * depolarization
+        * -np.sin(retardance)
+        * np.cos(2 * orientation)
+    )
+    s3 = transmittance * depolarization * np.cos(retardance)
     return s0, s1, s2, s3
 
 
-def stokes012_after_AR(ret, ori, tra, input="CPL"):
+def stokes012_after_ar(retardance, orientation, transmittance, input="cpl"):
     """
     Returns the first three Stokes parameters of the input polarization state
-    (default = circularly polarized light = "CPL") that has passed through an
-    attenuating retarder (AR) parametrized by its retardance (ret), slow-axis
-    orientation (ori), and transmittance (tra).
+    (default = circularly polarized light = "cpl") that has passed through an
+    attenuating retarder (ar) parametrized by its retardance, slow-axis
+    orientation, and transmittance.
 
     This model is used to model non-depolarizing samples, or situations where
     depolarization information is unavailable...e.g. when only a linear Stokes
@@ -187,12 +202,12 @@ def stokes012_after_AR(ret, ori, tra, input="CPL"):
 
     Parameters
     ----------
-    ret, ori, tra: array_like, identical shapes
-        ret: retardance of AR, 2*pi periodic
-        ori: slow-axis orientation of AR, 2*pi periodic
-        tra: transmittance of AR, 0 <= tra <= 1
+    retardance, orientation, transmittance: array_like, identical shapes
+        retardance: retardance of ar, 2*pi periodic
+        orientation: slow-axis orientation of ar, 2*pi periodic
+        transmittance: transmittance of ar, 0 <= transmittance <= 1
 
-    input : "CPL"
+    input : "cpl"
         Input polarization state
 
     Returns
@@ -201,23 +216,23 @@ def stokes012_after_AR(ret, ori, tra, input="CPL"):
         First three Stokes parameters
 
     """
-    if input != "CPL":
-        NotImplementedError("input != CPL")
+    if input != "cpl":
+        NotImplementedError("input != cpl")
 
-    # without copying tra, downstream changes to s0 will affect tra
-    s0 = np.array(tra).copy()
-    s1 = tra * np.sin(ret) * np.sin(2 * ori)
-    s2 = tra * -np.sin(ret) * np.cos(2 * ori)
+    # without copying transmittance, downstream changes to s0 will affect transmittance
+    s0 = np.array(transmittance).copy()
+    s1 = transmittance * np.sin(retardance) * np.sin(2 * orientation)
+    s2 = transmittance * -np.sin(retardance) * np.cos(2 * orientation)
     return s0, s1, s2
 
 
-def _s12_to_ori(s1, s2):
+def _s12_to_orientation(s1, s2):
     """
     Converts s1 and s2 into a slow-axis orientation.
 
-    This functions matches the sign convention used in s0123_CPL_after_ADR and
-    s012_CPL_after_AR (see tests for examples), and matches the orientation
-    range convention used in comp-micro: 0 <= ori < pi.
+    This functions matches the sign convention used in s0123_cpl_after_adr and
+    s012_cpl_after_ar (see tests for examples), and matches the orientation
+    range convention used in comp-micro: 0 <= orientation < pi.
 
     Parameters
     ----------
@@ -227,63 +242,63 @@ def _s12_to_ori(s1, s2):
     Returns
     -------
     array_like
-        Slow-axis orientation with 0 <= ori < pi.
+        Slow-axis orientation with 0 <= orientation < pi.
     """
     return (np.arctan2(s1, -s2) % (2 * np.pi)) / 2
 
 
-def estimate_ADR_from_stokes(s0, s1, s2, s3, input="CPL"):
+def estimate_adr_from_stokes(s0, s1, s2, s3, input="cpl"):
     """
-    Inverse of stokes_after_ADR.
+    Inverse of stokes_after_adr.
 
     When light with input polarization state (default = circularly polarized
-    light = "CPL") has passed through an attenuating depolarizing retarder
-    (ADR), its Stokes parameters can be passed to this function to estimate
-    the parameters of the ADR, specifically its retardance (ret), slow-axis
-    orientation (ori), transmittance (tra), and depolarization (dop).
+    light = "cpl") has passed through an attenuating depolarizing retarder
+    (adr), its Stokes parameters can be passed to this function to estimate
+    the parameters of the adr, specifically its retardance, slow-axis
+    orientation, transmittance, and depolarization.
 
     Note: this function is commonly used in QLIPP-type reconstructions. After
     converting raw intensities into Stokes parameters by applying an
     I2S_matrix, the Stokes parameters are used to estimate the parameters of
-    the sample ADR in a single step with this function.
+    the sample adr in a single step with this function.
 
     Parameters
     ----------
     s0, s1, s2, s3: array_like, identical shapes
         Stokes parameters
 
-    input : "CPL"
+    input : "cpl"
         Input polarization state
 
     Returns
     ----------
-    ret, ori, tra, dop: array_like
-        ret: retardance of ADR, 2*pi periodic
-        ori: slow-axis orientation of ADR, 2*pi periodic
-        tra: transmittance of ADR, 0 <= tra <= 1
-        dop: depolarization of ADR, 0 <= dop <= 1
+    retardance, orientation, transmittance, depolarization: array_like
+        retardance: retardance of adr, 2*pi periodic
+        orientation: slow-axis orientation of adr, 2*pi periodic
+        transmittance: transmittance of adr, 0 <= transmittance <= 1
+        depolarization: depolarization of adr, 0 <= depolarization <= 1
     """
-    if input != "CPL":
-        NotImplementedError("input != CPL")
+    if input != "cpl":
+        NotImplementedError("input != cpl")
 
     len_pol = (s1**2 + s2**2 + s3**2) ** 0.5
-    ret = np.arcsin(((s1**2 + s2**2) ** 0.5) / len_pol)
-    ori = _s12_to_ori(s1, s2)
-    # without copying s0, downstream changes to tra will affect s0
-    tra = np.array(s0).copy()
-    dop = len_pol / s0
-    return ret, ori, tra, dop
+    retardance = np.arcsin(((s1**2 + s2**2) ** 0.5) / len_pol)
+    orientation = _s12_to_orientation(s1, s2)
+    # without copying s0, downstream changes to transmittance will affect s0
+    transmittance = np.array(s0).copy()
+    depolarization = len_pol / s0
+    return retardance, orientation, transmittance, depolarization
 
 
-def estimate_AR_from_stokes012(s0, s1, s2, input="CPL"):
+def estimate_ar_from_stokes012(s0, s1, s2, input="cpl"):
     """
-    Inverse of stokes012_after_ADR.
+    Inverse of stokes012_after_adr.
 
     When light with input polarization state (default = circularly polarized
-    light = "CPL") has passed through an attenuating retarder (AR), its Stokes
+    light = "cpl") has passed through an attenuating retarder (ar), its Stokes
     parameters can be passed to this function to estimate the parameters of
-    the AR, specifically its retardance (ret), slow-axis orientation (ori), and
-    transmittance (tra).
+    the ar, specifically its retardance, slow-axis orientation, and
+    transmittance.
 
     Parameters
     ----------
@@ -292,19 +307,19 @@ def estimate_AR_from_stokes012(s0, s1, s2, input="CPL"):
 
     Returns
     ----------
-    ret, ori, tra: array_like, identical shapes
-        ret: retardance of AR, 2*pi periodic
-        ori: slow-axis orientation of AR, 2*pi periodic
-        tra: transmittance of AR, 0 <= tra <= 1
+    retardance, orientation, transmittance: array_like, identical shapes
+        retardance: retardance of ar, 2*pi periodic
+        orientation: slow-axis orientation of ar, 2*pi periodic
+        transmittance: transmittance of ar, 0 <= transmittance <= 1
     """
-    if input != "CPL":
-        NotImplementedError("input != CPL")
+    if input != "cpl":
+        NotImplementedError("input != cpl")
 
-    ret = np.arcsin(((s1**2 + s2**2) ** 0.5) / s0)
-    ori = _s12_to_ori(s1, s2)
-    # without copying s0, downstream changes to tra will affect s0
-    tra = np.array(s0).copy()
-    return ret, ori, tra
+    retardance = np.arcsin(((s1**2 + s2**2) ** 0.5) / s0)
+    orientation = _s12_to_orientation(s1, s2)
+    # without copying s0, downstream changes to transmittance will affect s0
+    transmittance = np.array(s0).copy()
+    return retardance, orientation, transmittance
 
 
 def mueller_from_stokes(
@@ -312,14 +327,14 @@ def mueller_from_stokes(
     s1,
     s2,
     s3,
-    input="CPL",
-    model="AR",
+    input="cpl",
+    model="ar",
     direction="inverse",
 ):
     """
     When light with input polarization state (default = circularly polarized
-    light = "CPL") has passed through a polarization element of a specific type
-    (default = attenuating retarder = "AR"), its Stokes parameters can be
+    light = "cpl") has passed through a polarization element of a specific type
+    (default = attenuating retarder = "ar"), its Stokes parameters can be
     passed to this function to estimate the complete Mueller matrix of the
     polarization element.
 
@@ -328,10 +343,10 @@ def mueller_from_stokes(
     s0, s1, s2, s3 : array_like, identical shapes
         Stokes parameters
 
-    input : "CPL"
+    input : "cpl"
         Input polarization state
 
-    model : "AR"
+    model : "ar"
         The type of polarization element
 
     direction : "forward" or "inverse"
@@ -342,11 +357,11 @@ def mueller_from_stokes(
     array_like, float, M.shape = (4, 4,) + s0.shape
         Mueller matrix
     """
-    if input != "CPL":
-        NotImplementedError("input != CPL")
+    if input != "cpl":
+        NotImplementedError("input != cpl")
 
-    if model != "AR":
-        NotImplementedError("input != AR")
+    if model != "ar":
+        NotImplementedError("input != ar")
 
     if not (direction == "forward" or direction == "inverse"):
         NotImplementedError("direction must be `forward` or `inverse`")
@@ -369,9 +384,9 @@ def mueller_from_stokes(
     elif direction == "inverse":
         """
         NOTE: this implementation calculates the full matrix then inverts it.
-        TODO: (medium) Calculate the inverse matrix directly here and exploit
+        TODO: (medium) calculate the inverse matrix directly here and exploit
         the block-diagonal structure.
-        TODO: (harder) Instead of calculating the entire inverse matrix and
+        TODO: (harder) instead of calculating the entire inverse matrix and
         using matrix multiplication to apply the correction, only calculate the
         unique terms (exploit the fact that the lower block is a scaled
         rotation matrix) and write a function that applies the correction
@@ -389,7 +404,7 @@ def mueller_from_stokes(
 def mmul(matrix, vector):
     """Convenient matrix-multiply used for
         - applying Mueller matrices to Stokes vectors
-        - applying S2I matrices to intensities
+        - applying intensity_to_stokes matrices to intensities
 
     Parameters
     ----------
