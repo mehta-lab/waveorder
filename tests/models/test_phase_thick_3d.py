7e992915a3e71f1f5a90afa7cbc819a4403a690e
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-06-09 16:12:08 -0700

    test axial flip

--- tests/models/test_phase_thick_3d.py
+++ tests/models/test_phase_thick_3d.py
@@ -1,7 +1,9 @@
+import pytest
 from waveorder.models import phase_thick_3d
 
 
-def test_calculate_transfer_function():
+@pytest.mark.parametrize("axial_flip", (True, False))
+def test_calculate_transfer_function(axial_flip):
     z_padding = 5
     H_re, H_im = phase_thick_3d.calculate_transfer_function(
         zyx_shape=(20, 100, 101),
@@ -12,6 +14,7 @@ def test_calculate_transfer_function():
         index_of_refraction_media=1.0,
         numerical_aperture_illumination=0.45,
         numerical_aperture_detection=0.55,
+        axial_flip=axial_flip
     )
 
     assert H_re.shape == (20 + 2 * z_padding, 100, 101)
