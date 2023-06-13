0a006f65b1ca7f54c6f51b22ade80cbc2740bc00
Talon Chandler <talonchandler@gmail.com>
2023-06-13 13:13:51 -0700

    prototype transfer function

--- tests/models/test_isotropic_fluorescent_thick_3d.py
+++ tests/models/test_isotropic_fluorescent_thick_3d.py
@@ -1,1 +1,21 @@
-# TODO
+import pytest
+from waveorder.models import isotropic_fluorescent_thick_3d
+
+
+@pytest.mark.parametrize("axial_flip", (True, False))
+def test_calculate_transfer_function(axial_flip):
+    z_padding = 5
+    transfer_function = (
+        isotropic_fluorescent_thick_3d.calculate_transfer_function(
+            zyx_shape=(20, 100, 101),
+            yx_pixel_size=6.5 / 40,
+            z_pixel_size=2,
+            wavelength_illumination=0.5,
+            z_padding=z_padding,
+            index_of_refraction_media=1.0,
+            numerical_aperture_detection=0.55,
+            axial_flip=axial_flip,
+        )
+    )
+
+    assert transfer_function.shape == (20 + 2 * z_padding, 100, 101)
--- waveorder/models/isotropic_fluorescent_thick_3d.py
+++ waveorder/models/isotropic_fluorescent_thick_3d.py
@@ -1,9 +1,57 @@
-def generate_test_phantom():
-    return NotImplementedError
+import torch
+from waveorder import optics, util
 
 
-def calculate_transfer_function():
-    return NotImplementedError
+def generate_test_phantom(yx_shape):
+    star, _, _ = util.generate_star_target(yx_shape, blur_px=0.1)
+    return star
+
+
+def calculate_transfer_function(
+    zyx_shape,
+    yx_pixel_size,
+    z_pixel_size,
+    wavelength_illumination,
+    z_padding,
+    index_of_refraction_media,
+    numerical_aperture_detection,
+    axial_flip=False,
+):
+    radial_frequencies = util.generate_radial_frequencies(
+        zyx_shape[1:], yx_pixel_size
+    )
+
+    z_total = zyx_shape[0] + 2 * z_padding
+    z_position_list = torch.fft.ifftshift(
+        (torch.arange(z_total) - z_total // 2) * z_pixel_size
+    )
+    if axial_flip:
+        z_position_list = torch.flip(z_position_list, dims=(0,))
+
+    det_pupil = optics.generate_pupil(
+        radial_frequencies,
+        numerical_aperture_detection,
+        wavelength_illumination,
+    )
+
+    propagation_kernel = optics.generate_propagation_kernel(
+        radial_frequencies,
+        det_pupil,
+        wavelength_illumination / index_of_refraction_media,
+        z_position_list,
+    )
+
+    point_spread_function = (
+        torch.abs(torch.fft.ifft2(propagation_kernel, dim=(0, 1))) ** 2
+    )
+    optical_transfer_function = torch.fft.fftn(
+        point_spread_function, dim=(0, 1, 2)
+    )
+    optical_transfer_function /= torch.max(
+        torch.abs(optical_transfer_function)
+    )  # normalize
+
+    return optical_transfer_function
 
 
 def visualize_transfer_function():
