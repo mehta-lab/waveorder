dbf96821d7f9025ebebff02cda32eaaa8281a963
Talon Chandler <talonchandler@gmail.com>
2023-06-13 12:41:39 -0700

    model outline

---
+++ waveorder/models/isotropic_fluorescent_thick_3d.py
@@ -0,0 +1,18 @@
+def generate_test_phantom():
+    return NotImplementedError
+
+
+def calculate_transfer_function():
+    return NotImplementedError
+
+
+def visualize_transfer_function():
+    return NotImplementedError
+
+
+def apply_transfer_function():
+    return NotImplementedError
+
+
+def apply_inverse_transfer_function():
+    return NotImplementedError
