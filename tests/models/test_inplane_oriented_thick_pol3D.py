c645c8474c55e22d650aabb07f45163bf3848bbb
Talon Chandler <talonchandler@gmail.com>
2023-06-14 08:30:09 -0700

    Rename model from `anisotropic_thin` to `oriented_thick` (#127)
    `anisotropic_thin` -> `oriented_thick`

--- tests/models/test_inplane_oriented_thick_pol3D.py
+++ tests/models/test_inplane_oriented_thick_pol3D.py
@@ -1,12 +1,12 @@
 import pytest
 import torch
 from waveorder import stokes
-from waveorder.models import inplane_anisotropic_thin_pol3d
+from waveorder.models import inplane_oriented_thick_pol3d
 
 
 def test_calculate_transfer_function():
     intensity_to_stokes_matrix = (
-        inplane_anisotropic_thin_pol3d.calculate_transfer_function(
+        inplane_oriented_thick_pol3d.calculate_transfer_function(
             swing=0.1,
             scheme="5-State",
         )
