165d100ed4afcdb882d454fba34eafe08540e637
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-08-03 13:40:15 -0700

    Bump dependencies and enable widget test (#391)
    * use Qt6 for all platforms
    
    * bump iohub
    
    * enable dock widget test on linux CI runners

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -1,17 +1,8 @@
-import sys
-import os
-
-import pytest
 from napari.viewer import ViewerModel
 
 from recOrder.plugin.main_widget import MainWidget
 
 
-# FIXME: Linux Qt platform in GitHub actions
-@pytest.mark.skipif(
-    bool("linux" in sys.platform and os.environ.get("CI")),
-    reason="Qt on Linux CI runners does not work",
-)
 def test_dock_widget(make_napari_viewer):
     viewer: ViewerModel = make_napari_viewer()
     viewer.window.add_dock_widget(MainWidget(viewer))
