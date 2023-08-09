1367a35194270e3f99f8a4d6d8bc2ef99b92db99
Talon Chandler <talonchandler@gmail.com>
2023-08-09 08:45:21 -0700

    remove `recorder` view (#400)

--- recOrder/cli/main.py
+++ recOrder/cli/main.py
@@ -1,8 +1,8 @@
 import click
-from recOrder.cli.view import view
-from recOrder.cli.reconstruct import reconstruct
-from recOrder.cli.compute_transfer_function import compute_tf
+
 from recOrder.cli.apply_inverse_transfer_function import apply_inv_tf
+from recOrder.cli.compute_transfer_function import compute_tf
+from recOrder.cli.reconstruct import reconstruct
 
 
 @click.group()
@@ -10,7 +10,6 @@ def cli():
     """\033[92mrecOrder: Computational Toolkit for Label-Free Imaging\033[0m\n"""
 
 
-cli.add_command(view)
 cli.add_command(reconstruct)
 cli.add_command(compute_tf)
 cli.add_command(apply_inv_tf)
