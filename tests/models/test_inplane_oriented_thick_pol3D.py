82eaeb9a3fe4b91c49c4b8545a2b71f527c7b03e
Talon Chandler <talonchandler@gmail.com>
2023-08-09 17:47:56 -0700

    basic apply inverse test

--- tests/models/test_inplane_oriented_thick_pol3D.py
+++ tests/models/test_inplane_oriented_thick_pol3D.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+
 from waveorder import stokes
 from waveorder.models import inplane_oriented_thick_pol3d
 
@@ -13,3 +14,25 @@ def test_calculate_transfer_function():
     )
 
     assert intensity_to_stokes_matrix.shape == (4, 5)
+
+
+def test_apply_inverse_transfer_function():
+    input_shape = (5, 10, 5, 5)
+    czyx_data = torch.randn(input_shape)
+
+    intensity_to_stokes_matrix = (
+        inplane_oriented_thick_pol3d.calculate_transfer_function(
+            swing=0.1,
+            scheme="5-State",
+        )
+    )
+
+    results = inplane_oriented_thick_pol3d.apply_inverse_transfer_function(
+        czyx_data=czyx_data,
+        intensity_to_stokes_matrix=intensity_to_stokes_matrix,
+    )
+
+    assert len(results) == 4
+
+    for result in results:
+        assert result.shape == input_shape[1:]
