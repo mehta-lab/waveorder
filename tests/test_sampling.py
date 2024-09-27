69fea21e6dd5b2a53337e72d7189218ece2fd8c1
Talon Chandler <talonchandler@gmail.com>
2024-09-27 11:05:18 -0700

    sampling tests

---
+++ tests/test_sampling.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+
+from waveorder.sampling import (
+    nd_fourier_central_cuboid,
+)
+
+
+def test_nd_fourier_central_cuboid():
+    source = torch.randn(8, 8)
+    target_shape = (4, 4)
+    result = nd_fourier_central_cuboid(source, target_shape)
+    assert result.shape == target_shape
