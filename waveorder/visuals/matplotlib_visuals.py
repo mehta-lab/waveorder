92bb6d646ca091027cb9824324d57c3a7925294d
Talon Chandler <talonchandler@gmail.com>
2024-10-14 22:20:10 -0700

    improved matplotlib visuals

--- waveorder/visuals/matplotlib_visuals.py
+++ waveorder/visuals/matplotlib_visuals.py
@@ -8,54 +8,63 @@ import os
 import torch
 
 
-def plot_transfer_function(
-    sfZYX_data,
+def plot_5d_ortho(
+    rczyx_data,
     filename,
     zyx_scale,
     z_slice,
-    s_labels,
-    f_labels,
+    row_labels,
+    column_labels,
     rose_path=None,
     inches_per_column=1,
     saturate_clim_fraction=1.0,
     trim_edges=0,
+    fourier_space=True,
 ):
-    S, F, Z, Y, X = sfZYX_data.shape
-    voxel_scale = [
+    R, C, Z, Y, X = rczyx_data.shape
+
+    axis_extent = [
         Z * zyx_scale[0],
         Y * zyx_scale[1],
         X * zyx_scale[2],
     ]
+    if fourier_space:
+        axis_extent = [1 / x for x in axis_extent]
 
-    sfZYX_data = nd_fourier_central_cuboid(
-        sfZYX_data, (S, F, Z - trim_edges, Y - trim_edges, X - trim_edges)
+    rczyx_data = nd_fourier_central_cuboid(
+        rczyx_data, (R, C, Z - trim_edges, Y - trim_edges, X - trim_edges)
     )
 
-    S, F, Z, Y, X = sfZYX_data.shape
-    sfZYX_data = torch.fft.ifftshift(sfZYX_data, dim=(-3, -2, -1))
-    sfZYX_rgb = complex_tensor_to_rgb(sfZYX_data, saturate_clim_fraction)
+    R, C, Z, Y, X = rczyx_data.shape  # after cropping
 
-    assert S == len(s_labels)
-    assert F == len(f_labels)
+    rczyx_data = torch.fft.ifftshift(rczyx_data, dim=(-3, -2, -1))
+    sfZYX_rgb = complex_tensor_to_rgb(rczyx_data, saturate_clim_fraction)
 
-    X_size = 1 * inches_per_column
-    Y_size = (zyx_scale[2] / zyx_scale[1]) * inches_per_column
-    Z_size = (zyx_scale[2] / zyx_scale[0]) * inches_per_column
+    assert R == len(row_labels)
+    assert C == len(column_labels)
 
-
-    n_rows = S + 1
-    n_cols = (F * 3) + 1
-
-    height = n_rows
-    width = n_cols
+    n_rows = (R * 2) + 1
+    n_cols = (C * 2) + 1
 
     fig, axes = plt.subplots(
         n_rows,
         n_cols,
-        figsize=(width, height),
+        figsize=(n_cols, n_rows),
         gridspec_kw={
             "wspace": 0.1,  # Adjust this value to reduce space between columns
             "hspace": 0.1,  # Adjust this value to reduce space between rows
+            "width_ratios": [1]
+            + C
+            * [
+                1,
+                Z / X,
+            ],
+            "height_ratios": [1]
+            + R
+            * [
+                1,
+                Z / Y,
+            ],
         },
     )
 
@@ -67,14 +76,14 @@ def plot_transfer_function(
 
     for i in range(n_rows):
         for j in range(n_cols):
-            if (i == 0 and (j - 1) % 3 == 1) or (j == 0 and i > 0):
+            if (i == 0 and (j - 1) % 2 == 0) or (j == 0 and (i - 1) % 2 == 0):
                 if i == 0:
                     folder = "gellman"
-                    index = f_labels[int((j - 1) / 3)]
+                    index = column_labels[int((j - 1) / 2)]
                 else:
                     folder = "stokes"
-                    index = s_labels[i - 1]
-                
+                    index = row_labels[int((i - 1) / 2)]
+
                 if isinstance(index, int):
                     image_path = os.path.join(
                         os.path.dirname(__file__),
@@ -94,21 +103,31 @@ def plot_transfer_function(
                     )
 
             if i > 0 and j > 0:
-                if (j - 1) % 3 == 0:
+                # XY
+                if (i - 1) % 2 == 0 and (j - 1) % 2 == 0:
                     axes[i, j].imshow(
-                        sfZYX_rgb[i - 1, int((j - 1) / 3), (Z // 2) + z_slice],
-                        aspect=voxel_scale[1] / voxel_scale[2],
-                        #interpolation=None
+                        sfZYX_rgb[
+                            int((i - 1) / 2),
+                            int((j - 1) / 2),
+                            (Z // 2) + z_slice,
+                        ],
+                        aspect=axis_extent[1] / axis_extent[2],
                     )
-                elif (j - 1) % 3 == 1:
+                # YZ
+                elif (i - 1) % 2 == 0 and (j - 1) % 2 == 1:
                     axes[i, j].imshow(
-                        sfZYX_rgb[i - 1, int((j - 1) / 3), :, :, X // 2, :],
-                        aspect=voxel_scale[2] / voxel_scale[0],
+                        sfZYX_rgb[
+                            int((i - 1) / 2), int((j - 1) / 2), :, :, X // 2, :
+                        ].transpose(1, 0, 2),
+                        aspect=axis_extent[1] / axis_extent[0],
                     )
-                elif (j - 1) % 3 == 2:
+                # XZ
+                elif (i - 1) % 2 == 1 and (j - 1) % 2 == 0:
                     axes[i, j].imshow(
-                        sfZYX_rgb[i - 1, int((j - 1) / 3), :, Y // 2],
-                        aspect=voxel_scale[1] / voxel_scale[0],
+                        sfZYX_rgb[
+                            int((i - 1) / 2), int((j - 1) / 2), :, Y // 2
+                        ],
+                        aspect=axis_extent[0] / axis_extent[1],
                     )
 
             axes[i, j].tick_params(
@@ -120,8 +139,11 @@ def plot_transfer_function(
             bottom = axes[-1, -1].get_position().y0
             left = axes[0, 0].get_position().x0
             right = axes[-1, -1].get_position().x1
-            if i == 0 and (j - 1) % 3 == 0:
-                left_edge = (axes[0, j].get_position().x0 + axes[0, j - 1].get_position().x1)/2
+            if i == 0 and (j - 1) % 2 == 0:
+                left_edge = (
+                    axes[0, j].get_position().x0
+                    + axes[0, j - 1].get_position().x1
+                ) / 2
                 fig.add_artist(
                     plt.Line2D(
                         [left_edge, left_edge],
@@ -131,8 +153,11 @@ def plot_transfer_function(
                         lw=0.5,
                     )
                 )
-            if j == 0 and i > 0:
-                top_edge = (axes[i, 0].get_position().y1 + axes[i - 1, 0].get_position().y0)/2
+            if j == 0 and (i - 1) % 2 == 0:
+                top_edge = (
+                    axes[i, 0].get_position().y1
+                    + axes[i - 1, 0].get_position().y0
+                ) / 2
                 fig.add_artist(
                     plt.Line2D(
                         [left, right],
@@ -148,22 +173,20 @@ def plot_transfer_function(
             axes[i, j].spines["bottom"].set_visible(False)
             axes[i, j].spines["left"].set_visible(False)
 
-     # Draw ortho view lines
+    # Draw ortho view lines
     fig.add_artist(
         plt.Line2D(
-            #[(X/2) - 0.5*X/np.sqrt(2) , (X/2) + 0.5*X/np.sqrt(2)],
-            #[(Y/2) - 0.5*Y/np.sqrt(2) , (Y/2) + 0.5*Y/np.sqrt(2)],  
-            [Y//2, Y//2],
+            [Y // 2, Y // 2],
             [0, X],
             transform=axes[1, 1].transData,  # use axis coordinates
             color="red",
             lw=0.5,
         )
     )
-    fig.add_artist(                    
+    fig.add_artist(
         plt.Line2D(
             [0, X],
-            [Y//2, Y//2],  # from bottom to top in axis coordinates
+            [Y // 2, Y // 2],  # from bottom to top in axis coordinates
             transform=axes[1, 1].transData,  # use axis coordinates
             color="blue",
             lw=0.5,
@@ -175,41 +198,48 @@ def plot_transfer_function(
             X,  # width
             Y,  # height
             linewidth=0.5,
-            edgecolor='green',
-            facecolor='none',
-            transform=axes[1, 1].transData  # use axis coordinates
+            edgecolor="green",
+            facecolor="none",
+            transform=axes[1, 1].transData,  # use axis coordinates
         )
     )
     axes[1, 1].text(
-        0.1, 0.975, 'x', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 1].transAxes, 
-        fontsize=5, 
-        color='black'
+        0.1,
+        0.975,
+        "x",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[1, 1].transAxes,
+        fontsize=5,
+        color="black",
     )
     axes[1, 1].text(
-        0.025, 0.9, 'y', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 1].transAxes, 
-        fontsize=5, 
-        color='black'
+        0.025,
+        0.9,
+        "y",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[1, 1].transAxes,
+        fontsize=5,
+        color="black",
     )
-    
-    fig.add_artist(                    
+
+    fig.add_artist(
         plt.Line2D(
-            [0, X],
-            [Z//2 + z_slice, Z//2 + z_slice],  # from bottom to top in axis coordinates
+            [
+                Z // 2 + z_slice,
+                Z // 2 + z_slice,
+            ],
+            [0, Y],
             transform=axes[1, 2].transData,  # use axis coordinates
             color="green",
             lw=0.5,
         )
     )
-    fig.add_artist(                    
+    fig.add_artist(
         plt.Line2D(
-            [X//2, X//2],
             [0, Z],  # from bottom to top in axis coordinates
+            [Y // 2, Y // 2],
             transform=axes[1, 2].transData,  # use axis coordinates
             color="blue",
             lw=0.5,
@@ -218,47 +248,53 @@ def plot_transfer_function(
 
     rect = plt.Rectangle(
         (0, 0),  # lower-left corner
-        X,  # width
-        Z,  # height
+        Z,  # width
+        Y,  # height
         linewidth=0.5,
-        edgecolor='red',
-        facecolor='none',
-        transform=axes[1, 2].transData  # use axis coordinates
+        edgecolor="red",
+        facecolor="none",
+        transform=axes[1, 2].transData,  # use axis coordinates
     )
     fig.add_artist(rect)
 
     axes[1, 2].text(
-        0.1, 0.975, 'y', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 2].transAxes, 
-        fontsize=5, 
-        color='black'
+        0.1,
+        0.975,
+        "z",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[1, 2].transAxes,
+        fontsize=5,
+        color="black",
     )
     axes[1, 2].text(
-        0.025, 0.9, 'z', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 2].transAxes, 
-        fontsize=5, 
-        color='black'
+        0.025,
+        0.9,
+        "y",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[1, 2].transAxes,
+        fontsize=5,
+        color="black",
     )
-    
 
-    fig.add_artist(                    
+    fig.add_artist(
         plt.Line2D(
             [0, X],
-            [Z//2 + z_slice, Z//2 + z_slice],  # from bottom to top in axis coordinates
-            transform=axes[1, 3].transData,  # use axis coordinates
+            [
+                Z // 2 + z_slice,
+                Z // 2 + z_slice,
+            ],  # from bottom to top in axis coordinates
+            transform=axes[2, 1].transData,  # use axis coordinates
             color="green",
             lw=0.5,
         )
     )
-    fig.add_artist(                    
+    fig.add_artist(
         plt.Line2D(
-            [X//2, X//2],
+            [X // 2, X // 2],
             [0, Z],  # from bottom to top in axis coordinates
-            transform=axes[1, 3].transData,  # use axis coordinates
+            transform=axes[2, 1].transData,  # use axis coordinates
             color="red",
             lw=0.5,
         )
@@ -269,27 +305,31 @@ def plot_transfer_function(
         X,  # width
         Z,  # height
         linewidth=0.5,
-        edgecolor='blue',
-        facecolor='none',
-        transform=axes[1, 3].transData  # use axis coordinates
+        edgecolor="blue",
+        facecolor="none",
+        transform=axes[2, 1].transData,  # use axis coordinates
     )
     fig.add_artist(rect)
 
-    axes[1, 3].text(
-        0.1, 0.975, 'x', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 3].transAxes, 
-        fontsize=5, 
-        color='black'
+    axes[2, 1].text(
+        0.1,
+        0.975,
+        "x",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[2, 1].transAxes,
+        fontsize=5,
+        color="black",
     )
-    axes[1, 3].text(
-        0.025, 0.9, 'z', 
-        horizontalalignment='left', 
-        verticalalignment='top', 
-        transform=axes[1, 3].transAxes, 
-        fontsize=5, 
-        color='black'
+    axes[2, 1].text(
+        0.025,
+        0.9,
+        "z",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=axes[2, 1].transAxes,
+        fontsize=5,
+        color="black",
     )
 
     print(f"Saving {filename}")
