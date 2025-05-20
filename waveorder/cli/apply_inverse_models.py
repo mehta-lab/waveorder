d1ae68dc8b78bb76440902d843a7ae6b5fbaf5f6
Talon Chandler <talonchandler@gmail.com>
2025-05-19 17:23:03 -0700

    `tensor` to `from_numpy`

--- waveorder/cli/apply_inverse_models.py
+++ waveorder/cli/apply_inverse_models.py
@@ -65,9 +65,13 @@ def phase(
     # [phase only, 2]
     if recon_dim == 2:
         # Load transfer functions
-        U = torch.tensor(transfer_function_dataset["singular_system_U"][0])
-        S = torch.tensor(transfer_function_dataset["singular_system_S"][0, 0])
-        Vh = torch.tensor(transfer_function_dataset["singular_system_Vh"][0])
+        U = torch.from_numpy(transfer_function_dataset["singular_system_U"][0])
+        S = torch.from_numpy(
+            transfer_function_dataset["singular_system_S"][0, 0]
+        )
+        Vh = torch.from_numpy(
+            transfer_function_dataset["singular_system_Vh"][0]
+        )
 
         # Apply
         (
