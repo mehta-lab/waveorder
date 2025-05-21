fix sign flipping bug...allow `z_focus_offset` to interact with `invert_phase_contrast` correctly

waveorder/cli/compute_transfer_function.py
@@ -69,7 +69,7 @@ def generate_and_save_phase_transfer_function(
         settings_dict["z_position_list"] = list(
             -(
                 np.arange(zyx_shape[0])
-                + settings_dict["z_focus_offset"]
+                - settings_dict["z_focus_offset"]
                 - (zyx_shape[0] // 2)
             )
             * settings_dict["z_pixel_size"]

waveorder/models/isotropic_thin_3d.py
@@ -105,8 +105,7 @@ def _calculate_wrap_unsafe_transfer_function(
     invert_phase_contrast: bool = False,
 ) -> Tuple[Tensor, Tensor]:
     if invert_phase_contrast:
-        z_position_list = torch.flip(torch.tensor(z_position_list), dims=(0,))
-
+        z_position_list = [-1 * x for x in z_position_list]
     radial_frequencies = util.generate_radial_frequencies(
         yx_shape, yx_pixel_size
     )