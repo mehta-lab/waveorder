8623dcf40f1cc5ba2a0c38e12ddec60b04d1cbb9
Amitabh Verma <averma@mbl.edu>
2025-08-24 03:51:37 -0500

    Update fix_md_links.py
    - fixes other types of github vid links for vid copy/link replacement

--- docs/scripts/fix_md_links.py
+++ docs/scripts/fix_md_links.py
@@ -15,6 +15,21 @@ ultimate_replacements = [
         "figure-slideshow.mp4",
         False,
     ],
+    [
+        "https://user-images.githubusercontent.com/9554101/271128301-cc71da57-df6f-401b-a955-796750a96d88.mov",
+        "2023-05-zebrafish.mov",
+        False,
+    ],
+    [
+        "https://user-images.githubusercontent.com/9554101/271128510-aa2180af-607f-4c0c-912c-c18dc4f29432.mp4",
+        "2023-08-zebrafish-embryo.mp4",
+        False,
+    ],
+    [
+        "https://user-images.githubusercontent.com/9554101/273073475-70afb05a-1eb7-4019-9c42-af3e07bef723.mp4",
+        "2023-10-05-recOrder-build-v2.mp4",
+        False,
+    ],
 ]
 
 
@@ -25,8 +40,12 @@ def fix_markdown_links(html_file):
         content = re.sub(r"(href=\".*?).md", r"\1.html", content)
         # Example: Replace relative paths that point to -- with - for html targets
         content = re.sub(r"(?<=[\w])--(?=[\w])", r"-", content)
-        # Replace : ultimate_replacements
+        # Replace : ultimate_replacements - github user-attachments
         content = replace_github_videos(content)
+        # Replace : ultimate_replacements - githubusercontent
+        content = replace_github_videos1(content)
+        # Replace : ultimate_replacements - githubusercontent
+        content = replace_github_videos2(content)
         f.seek(0)
         f.write(content)
         f.truncate()
@@ -39,15 +58,68 @@ def replace_github_videos(content: str):
     pre_fin = '<video src="https://waveorder.readthedocs.io/en/latest/_static/videos/'
     post_fin = '" controls autoplay></video>'
     for replacements in ultimate_replacements:
-        src_txt = pre_src + replacements[0] + post_src
-        if src_txt in content:
-            fin_txt = pre_fin + replacements[1] + post_fin
-            content = content.replace(src_txt, fin_txt)
-            print(f"Replacing '{src_txt}' with '{fin_txt}'")
-            if not replacements[2]:
-                success = download_video(replacements[0], replacements[1])
-                if success:
-                    replacements[2] = True
+        if not replacements[2]:
+            src_txt = pre_src + replacements[0] + post_src
+            if src_txt in content:
+                fin_txt = pre_fin + replacements[1] + post_fin
+                content = content.replace(src_txt, fin_txt)
+                print(f"Replacing '{src_txt}' with '{fin_txt}'")
+                if not replacements[2]:
+                    success = download_video(replacements[0], replacements[1])
+                    if success:
+                        replacements[2] = True
+    return content
+
+
+# <a class="reference external" href="https://user-images.githubusercontent.com/9554101/271128301-cc71da57-df6f-401b-a955-796750a96d88.mov">https://user-images.githubusercontent.com/9554101/271128301-cc71da57-df6f-401b-a955-796750a96d88.mov</a>
+def replace_github_videos1(content: str):
+    pre_src = '<a class="reference external" href="'
+    post_src1 = '">'
+    post_src2 = "</a>"
+    pre_fin = '<video src="https://waveorder.readthedocs.io/en/latest/_static/videos/'
+    post_fin = '" controls autoplay></video>'
+    for replacements in ultimate_replacements:
+        if not replacements[2]:
+            src_txt = (
+                pre_src
+                + replacements[0]
+                + post_src1
+                + replacements[0]
+                + post_src2
+            )
+            if src_txt in content:
+                fin_txt = pre_fin + replacements[1] + post_fin
+                content = content.replace(src_txt, fin_txt)
+                print(f"Replacing '{src_txt}' with '{fin_txt}'")
+                if not replacements[2]:
+                    success = download_video(replacements[0], replacements[1])
+                    if success:
+                        replacements[2] = True
+    return content
+
+
+# <video src="https://private-user-images.githubusercontent.com/9554101/271128301-cc71da57-df6f-401b-a955-796750a96d88.mov?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTYwMjI2MDAsIm5iZiI6MTc1NjAyMjMwMCwicGF0aCI6Ii85NTU0MTAxLzI3MTEyODMwMS1jYzcxZGE1Ny1kZjZmLTQwMWItYTk1NS03OTY3NTBhOTZkODgubW92P1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDgyNCUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTA4MjRUMDc1ODIwWiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9ZjkyNjNmNjk5YjM1OGMwMGQ0ZmRkYzQzZGIxMzQzOTYwMjk1NTFiZmI1YjAyZGVjZTQxODU3M2RhYWMxODc0MiZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.XTy2FO_rXMopZznWriS984hYel1l_MINmz27pceoxzU" data-canonical-src="https://private-user-images.githubusercontent.com/9554101/271128301-cc71da57-df6f-401b-a955-796750a96d88.mov?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTYwMjI2MDAsIm5iZiI6MTc1NjAyMjMwMCwicGF0aCI6Ii85NTU0MTAxLzI3MTEyODMwMS1jYzcxZGE1Ny1kZjZmLTQwMWItYTk1NS03OTY3NTBhOTZkODgubW92P1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI1MDgyNCUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTA4MjRUMDc1ODIwWiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9ZjkyNjNmNjk5YjM1OGMwMGQ0ZmRkYzQzZGIxMzQzOTYwMjk1NTFiZmI1YjAyZGVjZTQxODU3M2RhYWMxODc0MiZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QifQ.XTy2FO_rXMopZznWriS984hYel1l_MINmz27pceoxzU" controls="controls" muted="muted" class="d-block rounded-bottom-2 border-top width-fit" style="max-height:640px; min-height: 200px"></video>
+def replace_github_videos2(content: str):
+    pre_fin = "https://waveorder.readthedocs.io/en/latest/_static/videos/"
+    post_fin = ""
+    for replacements in ultimate_replacements:
+        if not replacements[2]:
+            src_txt = replacements[0]
+            vid_links = re.finditer(
+                r"video src=\"(.*?)\"", content, re.MULTILINE
+            )
+            for vid_link in vid_links:
+                if src_txt in content:
+                    src_txt = vid_link
+                    fin_txt = pre_fin + replacements[1] + post_fin
+                    content = content.replace(src_txt, fin_txt)
+                    print(f"Replacing '{src_txt}' with '{fin_txt}'")
+                    if not replacements[2]:
+                        success = download_video(
+                            replacements[0], replacements[1]
+                        )
+                        if success:
+                            replacements[2] = True
     return content
 
 
