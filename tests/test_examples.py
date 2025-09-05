f1e3dbcf934787175bb6167aba35d1ac03ddc393
Talley Lambert <talley.lambert@gmail.com>
2025-09-05 11:38:53 -0400

    tests: run example tests on ci and mock show

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,65 +1,35 @@
-import os
-import subprocess
-import sys
+import runpy
+from pathlib import Path
+from unittest.mock import patch
 
+import matplotlib.pyplot as plt
 import pytest
 
-
-def _run_scripts(scripts):
-    for script in scripts:
-        path = os.path.join(os.getcwd(), "docs/examples/maintenance/", script)
-        completed_process = subprocess.run(
-            [sys.executable, path], env=os.environ
-        )
-        assert completed_process.returncode == 0
+DOCS = Path(__file__).parent.parent / "docs"
+EXAMPLES = DOCS / "examples"
 
 
-# Currently, these tests will run in the background if no screen is available
-# If you do have a screen available, plots will appear, and you'll need to
-# close them to make the tests pass.
-# TODO: see if we can make these run locally w/o showing matplotlib
-# @patch("matplotlib.pyplot.show")
-@pytest.mark.skipif(
-    os.getenv("GITHUB_ACTIONS") == "true", reason="Skip on GitHub Actions"
+@pytest.mark.parametrize(
+    "example",
+    [
+        EXAMPLES / "maintenance" / "QLIPP_simulation/2D_QLIPP_forward.py",
+        EXAMPLES / "maintenance" / "QLIPP_simulation/2D_QLIPP_recon.py",
+        EXAMPLES / "maintenance" / "PTI_simulation/PTI_Simulation_Forward_2D3D.py",
+        EXAMPLES / "maintenance" / "PTI_simulation/PTI_Simulation_Recon2D.py",
+        EXAMPLES / "maintenance" / "PTI_simulation/PTI_Simulation_Recon3D.py",
+        EXAMPLES / "models" / "isotropic_thin_3d.py",
+        EXAMPLES / "models" / "phase_thick_3d.py",
+        EXAMPLES / "models" / "inplane_oriented_thick_pol3d.py",
+    ],
+    ids=lambda p: p.name,
 )
-def test_qlipp_examples():
-    scripts = [
-        "QLIPP_simulation/2D_QLIPP_forward.py",
-        "QLIPP_simulation/2D_QLIPP_recon.py",
-    ]
-    _run_scripts(scripts)
+def test_examples(example):
+    # run script, mocking all the show and imshow calls
+    with (
+        patch("matplotlib.pyplot.show"),
+        patch("matplotlib.pyplot.imshow"),
+        patch("builtins.input"),
+    ):
+        runpy.run_path(example, run_name="__main__")
 
-
-@pytest.mark.skipif(
-    os.getenv("GITHUB_ACTIONS") == "true", reason="Skip on GitHub Actions"
-)
-def test_pti_examples():
-    scripts = [
-        "PTI_simulation/PTI_Simulation_Forward_2D3D.py",
-        "PTI_simulation/PTI_Simulation_Recon2D.py",
-        "PTI_simulation/PTI_Simulation_Recon3D.py",
-    ]
-    _run_scripts(scripts)
-
-
-@pytest.mark.skipif(
-    os.getenv("GITHUB_ACTIONS") == "true",
-    reason="Skip on GitHub Actions, requires napari",
-)
-def test_phase_examples():
-    scripts = [
-        "isotropic_thin_3d.py",
-        "phase_thick_3d.py",
-        "inplane_oriented_thick_pol3d.py",
-    ]
-
-    for script in scripts:
-        path = os.path.join(os.getcwd(), "docs/examples/models/", script)
-        # examples needs two <enters>s so send input="e\ne"
-        completed_process = subprocess.run(
-            [sys.executable, path],
-            input="e\ne",
-            encoding="ascii",
-            env=os.environ,
-        )
-        assert completed_process.returncode == 0
+    plt.close("all")
